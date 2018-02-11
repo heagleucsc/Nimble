7556c29afb34419662457c0a36a4618776eb38f9
sportzwiz321 <nmchen@ucsc.edu>
2018-02-11 15:14:21 -0800

    Created a functional flash card which will switch between the term and the definition

--- Nimble/src/app/flashcard/flashcard.component.ts
+++ Nimble/src/app/flashcard/flashcard.component.ts
@@ -12,4 +12,24 @@ export class FlashcardComponent implements OnInit {
   ngOnInit() {
   }
 
+  flipcard(): void {
+	console.log("Hello World!");
+	var term = document.getElementById("term");
+	console.log(term);
+	if(term.style.display == "block") {
+		term.style.display = "none";
+	} else {
+		term.style.display = "block";
+	}
+
+	var definition = document.getElementById("definition");
+	console.log(definition);
+	if(definition.style.display == "block") {
+		definition.style.display = "none";
+	} else {
+		definition.style.display = "block";
+	}
+
+  }
+
 }
