ea807e895987eb4d61398ee7a5ddceb7044f648d
krosengrant <krosengr@ucsc.edu>
2018-02-11 15:21:41 -0800

    Introduction to Icons page/component created

---
+++ Nimble/src/app/intro-icons/intro-icons.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-intro-icons',
+  templateUrl: './intro-icons.component.html',
+  styleUrls: ['./intro-icons.component.css']
+})
+export class IntroIconsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
