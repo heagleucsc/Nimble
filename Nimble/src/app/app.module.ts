df1104a459e0d915e28d1fd3b02bb789c4ff82df
sportzwiz321 <nmchen@ucsc.edu>
2018-02-11 14:45:35 -0800

    Generated the flashcard component

--- Nimble/src/app/app.module.ts
+++ Nimble/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { FlashcardComponent } from './flashcard/flashcard.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FlashcardComponent
   ],
   imports: [
     BrowserModule
