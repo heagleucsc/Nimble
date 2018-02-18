d0b8711a61bd32d0f8bc0124003974956dc6c1a9
Ray Chen <rchen25@ucsc.edu>
2018-02-18 14:34:09 -0800

    fixes ul padding and adds some functionality for checking answers (need to enter in key-value pairs and make logic after getting value)

--- Nimble/src/app/matching/matching.component.ts
+++ Nimble/src/app/matching/matching.component.ts
@@ -8,8 +8,12 @@ import { Component, OnInit } from '@angular/core';
 export class MatchingComponent implements OnInit {
 
   imgFolder: string = "../../assets/matching/imgs/";
+  answers: {[key: string]: string};
   left:string[] = ["", "", "", "", "", "", "", "", "", ""];
   right:string[] = ["", "", "", "", "", "", "", "", "", ""];
+  leftSelected:number;
+  rightSelected:number;
+  isRight:boolean;
 
   constructor() {
   }
@@ -17,6 +21,8 @@ export class MatchingComponent implements OnInit {
   ngOnInit() {
     this.initializeLeft(this.left);
     this.initializeRight(this.right);
+    this.leftSelected= -1;
+    this.rightSelected= -1;
   }
 
   initializeLeft(left:string[]): void {
@@ -29,6 +35,26 @@ export class MatchingComponent implements OnInit {
     for (let i in right) {
       right[i] = `[placeholder]`;
     }
+  }
+
+  selectLeft(index:number): void {
+    this.leftSelected = index;
+    if (this.rightSelected != -1)
+      this.isRight = this.evalAnswer(this.leftSelected, this.rightSelected);
+  }
+
+  selectRight(index:number): void {
+    this.rightSelected = index;
+    if (this.leftSelected != -1)
+      this.isRight = this.evalAnswer(this.leftSelected, this.rightSelected);
+  }
+
+  evalAnswer(idxL:number, idxR:number): boolean {
+    let result:boolean = this.answers[idxL] == this.right[idxR];
+    
+    
+
+    return result;
   }
 
   drawLine() {
