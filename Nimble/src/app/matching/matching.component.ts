885a1be221b78ca15f1a187053626deb169123a5
Ray Chen <rchen25@ucsc.edu>
2018-02-20 17:47:14 -0800

    reduced code and added button selection indicator

--- Nimble/src/app/matching/matching.component.ts
+++ Nimble/src/app/matching/matching.component.ts
@@ -8,101 +8,90 @@ import { ICONS } from '../../assets/matching/icons';
 })
 export class MatchingComponent implements OnInit {
 
-  imgFolder: string = "../../assets/matching/imgs/";
+  imgDir: string = "../../assets/matching/imgs/";
   left:string[] = ["", "", "", "", "", "", "", "", "", ""];
   right:string[] = ["", "", "", "", "", "", "", "", "", ""];
-  leftSelected:number;
-  rightSelected:number;
-  isCorrect:boolean;
+  leftSelected:number = -1;
+  rightSelected:number = -1;
 
   constructor() {
   }
 
   ngOnInit() {
-    this.initializeArrays(this.left, this.right);
-    this.initializeSelections();
+    this.initSelections();
+    this.initArrays();
+
     let img = document.getElementById("img");
     let text = document.getElementById("text");
     let imgbox = img.getBoundingClientRect();
     let textbox = text.getBoundingClientRect();
     let canvas = <HTMLCanvasElement> document.getElementById("myCanvas");
+
     canvas.width = textbox.left - imgbox.right - 92;
     canvas.height = 344;
   }
 
-  initializeSelections(): void {
-    this.leftSelected= -1;
-    this.rightSelected= -1;
+  initSelections(): void {
+    this.leftSelected = this.rightSelected = -1;
   }
 
-  initializeArrays(left:string[], right:string[]): void {
-    for (let i in left) {
+  initArrays(): void {
+    for (let i in this.left) {
       let currIcon:string = "";
       let randIcon:string;
 
-      while (currIcon == "") {
-        randIcon = this.randomIcon();
+      while (currIcon == "")
+        if (!this.left.includes(randIcon = this.randomIcon()))
+          currIcon = randIcon;
 
-        if (!left.includes(randIcon)) currIcon = randIcon;
-      }
-
-      left[i] = randIcon;
-      right[i] = ICONS[currIcon];
+      this.left[i] = randIcon;
+      this.right[i] = ICONS[currIcon];
     }
-    this.shuffle(left);
-    this.shuffle(right);
+    this.shuffle(this.left);
+    this.shuffle(this.right);
   }
 
   randomIcon(): string {
-    var keys = Object.keys(ICONS);
+    let keys = Object.keys(ICONS);
     return keys[keys.length * Math.random() << 0];
   }
 
   // function taken from https://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array
-  shuffle(a:string[]): void {
-    let j:number;
-    let x:string;
-    let i:number;
+  shuffle(arr:string[]): void {
+    let ran:number;
+    let ranVal:string;
+    let idx:number;
 
-    for (i = a.length - 1; i > 0; i--) {
-        j = Math.floor(Math.random() * (i + 1));
-        x = a[i];
-        a[i] = a[j];
-        a[j] = x;
+    for (idx = arr.length - 1; idx > 0; idx--) {
+      ran = Math.floor(Math.random() * (idx + 1));
+      ranVal = arr[idx];
+      arr[idx] = arr[ran];
+      arr[ran] = ranVal;
     }
-}
-
-  selectLeft(index:number): void {
-    this.leftSelected = index;
-    if (this.rightSelected != -1){
-    this.evalAnswer(this.leftSelected, this.rightSelected);
-	  }
   }
 
-  selectRight(index:number): void {
-    this.rightSelected = index;
-    if (this.leftSelected != -1){
-    this.evalAnswer(this.leftSelected, this.rightSelected);
-	  }
+  select(idx:number, side:string, other:string): void {
+    this[`${side}Selected`] = idx;
+    if (this[`${other}Selected`] !== -1) {
+      this.evalAnswer(this.leftSelected, this.rightSelected);
+    }
   }
 
   evalAnswer(idxL:number, idxR:number): void {
     let iconKey:string = this.left[idxL];
-    this.isCorrect = ICONS[iconKey] == this.right[idxR];
+    let isCorrect = ICONS[iconKey] === this.right[idxR];
 
-    this.drawLine(this.leftSelected, this.rightSelected, this.isCorrect);
-    this.initializeSelections();
+    this.drawLine(this.leftSelected, this.rightSelected, isCorrect);
+    this.initSelections();
   }
 
   drawLine(indexLeft:number, indexRight:number, isCorrect:boolean) {
-	  var leftY = 20 + indexLeft*34;
-	  var rightY = 21 + indexRight*34;
-	  
-	  var canvas = <HTMLCanvasElement> document.getElementById("myCanvas");
-	  var ctx = canvas.getContext("2d");
+	  let leftY = 20 + indexLeft * 34;
+	  let rightY = 21 + indexRight * 34;
+	  let canvas = <HTMLCanvasElement> document.getElementById("myCanvas");
+	  let ctx = canvas.getContext("2d");
 
     ctx.strokeStyle = isCorrect ? "#00FF00" : "#FF0000";
-
 	  ctx.beginPath();
 	  ctx.moveTo(0, leftY);
 	  ctx.lineTo(canvas.width, rightY);
