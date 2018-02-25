78805a71ff81376478b1d75c373a2453d4727dbb
sportzwiz321 <nmchen@ucsc.edu>
2018-02-25 13:41:58 -0800

    Modified the back card logic to properly rotate in the flashcard app. Created the basic implementation of the flashcards for the email app. Added the email app to the entire application.

--- Nimble/src/app/email/email.component.ts
+++ Nimble/src/app/email/email.component.ts
@@ -7,9 +7,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmailComponent implements OnInit {
 
+	keyword = [
+		["Ni hao", "Hello"],
+		["Zai jian", "Goodbye"],
+		["Wei shen me", "Why"],
+		["Hao jiu mei jian", "Long time no see"],
+		["Xie xie", "thank you"]
+	]
+
+  index = 0;
+  total_cards = this.keyword.length;
+
   constructor() { }
 
   ngOnInit() {
+  	document.getElementById("email term").innerHTML = "Term: " + this.keyword[this.index][0];
+  	document.getElementById("email definition").innerHTML = "Definition: " + this.keyword[this.index][1];
+  }
+
+  rotatecard(): void {
+  	this.index = (this.index + 1) % this.total_cards;
+  	document.getElementById("email term").innerHTML = "Term: " + this.keyword[this.index][0];
+  	document.getElementById("email definition").innerHTML = "Definition: " + this.keyword[this.index][1];
+	}
+	
+  backcard(): void {
+  	this.index = (this.index - 1 + this.total_cards) % this.total_cards;
+  	console.log(this.index);
+  	document.getElementById("email term").innerHTML = "Term: " + this.keyword[this.index][0];
+  	document.getElementById("email definition").innerHTML = "Definition: " + this.keyword[this.index][1];
+  }
+
+  flipcard(): void {
+	var term = document.getElementById("email term");
+	if(term.style.display == "block") {
+		term.style.display = "none";
+	} else {
+		term.style.display = "block";
+	}
+
+	var definition = document.getElementById("email definition");
+	if(definition.style.display == "block") {
+		definition.style.display = "none";
+	} else {
+		definition.style.display = "block";
+	}
+
   }
 
 }
--- Nimble/src/app/flashcard/flashcard.component.ts
+++ Nimble/src/app/flashcard/flashcard.component.ts
@@ -87,7 +87,7 @@ export class FlashcardComponent implements OnInit {
 	}
 	
 	backcard(): void {
-  	this.index = (this.index - 1) % this.total_cards;
+  	this.index = (this.index - 1 + this.total_cards) % this.total_cards;
   	document.getElementById("term").innerHTML = "Term: " + this.keyword[this.index][0];
   	document.getElementById("definition").innerHTML = "Definition: " + this.keyword[this.index][1];
   }
