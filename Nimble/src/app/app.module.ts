46148f7f1f64eccc5cc8571b8429c8a1d54c89b2
krosengrant <krosengr@ucsc.edu>
2018-03-06 14:04:50 -0800

    Added teach icon component which is currently just a complete list of icons in table format

--- Nimble/src/app/app.module.ts
+++ Nimble/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { IntroIconsComponent } from './intro-icons/intro-icons.component';
 import { IntroNimbleComponent } from './intro-nimble/intro-nimble.component';
 import { IntroTerminologyComponent } from './intro-terminology/intro-terminology.component';
 import { EmailRwcComponent } from './email-rwc/email-rwc.component';
+import { IconsTeachComponent } from './icons-teach/icons-teach.component';
 
 
 @NgModule({
@@ -19,7 +20,8 @@ import { EmailRwcComponent } from './email-rwc/email-rwc.component';
     IntroIconsComponent,
     IntroNimbleComponent,
     IntroTerminologyComponent,
-    EmailRwcComponent
+    EmailRwcComponent,
+    IconsTeachComponent
   ],
   imports: [
     BrowserModule
