a5aa723fa4a6342926ca3f57092926b33464fd97
Sean Corzine <scorzine@ucsc.edu>
2018-03-11 20:07:09 -0700

    initial desktop activity

--- Nimble/src/app/desktop-canvas/desktop-canvas.component.ts
+++ Nimble/src/app/desktop-canvas/desktop-canvas.component.ts
@@ -14,22 +14,17 @@ export class DesktopCanvasComponent implements OnInit {
     constructor() { }
 
     ngOnInit() {
-    	var bg = <HTMLImageElement> document.getElementById('bg');
-	    var canvas = <HTMLCanvasElement> document.getElementById('desktopCanvas'),
+    	let bg = <HTMLImageElement> document.getElementById('bg');
+	    let canvas = <HTMLCanvasElement> document.getElementById('desktopCanvas'),
 	        ctx = canvas.getContext('2d');
 	    canvas.height = bg.height;
 		  canvas.width = Math.round(bg.height*(bg.naturalWidth/bg.naturalHeight));
-
-	    // ctx.beginPath();
-	    // ctx.moveTo(0, 0);
-	    // ctx.lineTo(canvas.width, canvas.height);
-	    // ctx.stroke();
     }
 
-    nextImage() {
-    	var bg = <HTMLImageElement> document.getElementById('bg');
-    	var login = <HTMLImageElement> document.getElementById('login');
-    	bg.src = "../assets/blankDesktop.png";
+    login() {
+    	let bg = <HTMLImageElement> document.getElementById('bg');
+    	let login = <HTMLImageElement> document.getElementById('login');
+    	bg.src = "../assets/desktop-canvas/macDesktop.png";
   		login.parentNode.removeChild(login);
       this.icons = true;
     }
