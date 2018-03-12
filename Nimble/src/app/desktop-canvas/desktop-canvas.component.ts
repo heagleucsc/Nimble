2a44ec6bb464d84136c9f87f13c045069819332f
Sean Corzine <scorzine@ucsc.edu>
2018-03-11 17:52:22 -0700

    Base desktop interactions

---
+++ Nimble/src/app/desktop-canvas/desktop-canvas.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-desktop-canvas',
+  templateUrl: './desktop-canvas.component.html',
+  styleUrls: ['./desktop-canvas.component.css']
+})
+export class DesktopCanvasComponent implements OnInit {
+
+    icons = false;
+    files = false;
+    folder = false;
+  
+    constructor() { }
+
+    ngOnInit() {
+    	var bg = <HTMLImageElement> document.getElementById('bg');
+	    var canvas = <HTMLCanvasElement> document.getElementById('desktopCanvas'),
+	        ctx = canvas.getContext('2d');
+	    canvas.height = bg.height;
+		  canvas.width = Math.round(bg.height*(bg.naturalWidth/bg.naturalHeight));
+
+	    // ctx.beginPath();
+	    // ctx.moveTo(0, 0);
+	    // ctx.lineTo(canvas.width, canvas.height);
+	    // ctx.stroke();
+    }
+
+    nextImage() {
+    	var bg = <HTMLImageElement> document.getElementById('bg');
+    	var login = <HTMLImageElement> document.getElementById('login');
+    	bg.src = "../assets/blankDesktop.png";
+  		login.parentNode.removeChild(login);
+      this.icons = true;
+    }
+
+    openFinder() {
+      this.files = true;
+    }
+
+    openFolder() {
+      this.folder = true;
+    }
+
+}
